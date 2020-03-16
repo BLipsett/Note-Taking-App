36b09a89db4a885fbe00c42fa820f3497215af7a
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-03-15 20:32:37 -0400

    working note input

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,60 +1,61 @@
-// const db = require("./11-express/public/Develop/db");
-// const uuidv4 = require('uuid/v4')
-const fs = require("fs")
-//C:\Users\lipse\git\Note-Taker\11-express\public\Develop\db\db.json
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+let tableData = require("../Develope/db/db");
+
+
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
 
-    app.get("/api/notes", function (req, res) {
-        res.json(db);
+  app.get("/api/notes", function(req, res) {
+    res.json(tableData);
+  });
 
-    });
+  
 
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
 
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-    app.post("/api/notes", function (req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
-        let receivedNote = req.body;
-        let newID = uuidv4();
-        //console.log(newID);
-        receivedNote.id = newID;
+  app.post("/api/notes", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body parsing middleware
+    if (tableData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
 
-        db.push(receivedNote);
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
 
-        res.json(db);
-    });
+  app.post("/api/clear", function(req, res) {
+    // Empty out the arrays of data
+    tableData.length = 0;
+    
 
-    app.delete("/api/notes/:id", function (req, res) {
-        let dbArray = db.filter((item) => {
-            //console.log(typeof(parseInt(item.id)))
-            //console.log(typeof(parseInt(req.params)))
-            if (item.id === req.params.id) {
-                return false;
-            }
-            else {
-                return true;
-            }
-        })
-        db.push(dbArray)
-        // console.log(newArray)
-        console.log(dbArray)
-        res.json(db)
-    });
-
-}
+    res.json({ ok: true });
+  });
+};
